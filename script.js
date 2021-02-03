428bae1f96d085c8b321fa5142855e8ead3941ed
Erik <aortiz.erik@gmail.com>
2021-02-02 21:03:51 -0500

    Fix issue #2

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function createWindow() {
         show: false,
         width: 1024,
         height: 768,
-        icon: is.linux ? path.join(__dirname, 'media', 'icon.png') : undefined,
+        //icon: is.linux ? path.join(__dirname, 'media', 'icon.png') : undefined,
         titleBarStyle: 'hidden',
         webPreferences:{
             preload: path.join(__dirname, 'browser.js'),
@@ -37,13 +37,25 @@ function createWindow() {
 app.on('ready', function () {
     Menu.setApplicationMenu(appMenu);
     mainWindow = createWindow();
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
     const page = mainWindow.webContents;
 
     page.on('dom-ready', function () {
         page.insertCSS(fs.readFileSync(path.join(__dirname, 'browser.css'), 'utf8'));
         mainWindow.show();
+
+        var injectJs = `var b = document.createElement('button');
+        b.classList.add('yt-icon-button');
+        b.style.width = '30px';
+        b.style.height = '30px';
+        b.innerHTML = '<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope iron-icon" style="pointer-events: none; display: block; width: 24; height: 24;"><g class="style-scope iron-icon"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" class="style-scope iron-icon"></path></g></svg>'
+        b.onclick = function() { window.history.back() };
+        document.getElementById('center').prepend(b)`
+        
+        mainWindow.webContents.executeJavaScript(injectJs, function (result) {
+            console.log('back button injected!')
+        });
     });
 });
 app.on('window-all-closed', function () {
